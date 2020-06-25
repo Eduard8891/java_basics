eddc320fb61e29da2c2df68bb0bf82aaabd4af7e
Eduard555 <ololohov@gmail.com>
2020-06-25 15:36:02 +0500

    lesson 3-6 set and get Car

--- 03_ObjectsAndClasses/RoadController/src/core/Car.java
+++ 03_ObjectsAndClasses/RoadController/src/core/Car.java
@@ -8,6 +8,31 @@ public class Car
     public boolean hasVehicle;
     public boolean isSpecial;
 
+    public void setNumber (String number) {
+        this.number = number;
+    }
+
+    public String getNumber () {
+        return number;
+    }
+
+    public void setHeight (int height) {
+        this.height = height;
+    }
+
+    public int getHeight () {
+        return height;
+    }
+
+    public void setWeight (double weight) {
+        this.weight = weight;
+    }
+
+    public double getWeight () {
+        return weight;
+    }
+
+
     public String toString()
     {
         String special = isSpecial ? "СПЕЦТРАНСПОРТ " : "";
