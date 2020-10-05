35727e44a141460bf1da9bcd1478b56aca695d07
Eduard555 <ololohov@gmail.com>
2020-10-05 19:58:03 +0500

    4.1 HW

--- 04_NumbersStringsAndDates/IntegerExperiments/src/Main.java
+++ 04_NumbersStringsAndDates/IntegerExperiments/src/Main.java
@@ -1,15 +1,30 @@
+import java.util.Scanner;
+
 public class Main
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args){
+
+        sumDigits(12345);
+        sumDigits(10);
+        sumDigits(5059191);
+
         Container container = new Container();
         container.count += 7843;
 
     }
 
-    public Integer sumDigits(Integer number)
-    {
-        //@TODO: write code here
+    public static Integer sumDigits(Integer number){
+        String sLength1 = number.toString();
+        Integer vLength = Integer.valueOf(sLength1);
+        int numParse = Integer.parseInt(sLength1);
+        int iLength = sLength1.length();
+        int sum = 0;
+        for (int num = 0; num < iLength; num++) {
+            char p = sLength1.charAt(num);
+            int intP = p - '0';
+            sum = sum + intP;
+        }
+        System.out.println("Сумма чисел числа "+number+" равна "+sum);
         return 0;
     }
 }
