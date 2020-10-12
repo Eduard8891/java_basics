2dc6a8b499d3cfc43aebee7904804708e7cb86df
Eduard555 <ololohov@gmail.com>
2020-10-12 17:31:40 +0500

    HW 4.2 min/max values

--- 04_NumbersStringsAndDates/MinMaxValues/src/Main.java
+++ 04_NumbersStringsAndDates/MinMaxValues/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         System.out.println("Мин. значение перем. типа int = "+Integer.MIN_VALUE+" | Макс. значение перем. типа int = "+Integer.MAX_VALUE);
         System.out.println("Мин. значение перем. типа short = "+Short.MIN_VALUE+" | Макс. значение перем. типа short = "+Short.MAX_VALUE);
         System.out.println("Мин. значение перем. типа long = "+Long.MIN_VALUE+" | Макс. значение перем. типа long = "+Long.MAX_VALUE);
-        System.out.println("Мин. значение перем. типа float = "+Float.MIN_VALUE+" | Макс. значение перем. типа float = "+Float.MAX_VALUE);
-        System.out.println("Мин. значение перем. типа double = "+Double.MIN_VALUE+" | Макс. значение перем. типа double = "+Double.MAX_VALUE);
+        System.out.println("Мин. значение перем. типа float = "+-Float.MIN_VALUE+" | Макс. значение перем. типа float = "+Float.MAX_VALUE);
+        System.out.println("Мин. значение перем. типа double = "+-Double.MIN_VALUE+" | Макс. значение перем. типа double = "+Double.MAX_VALUE);
     }
 }
