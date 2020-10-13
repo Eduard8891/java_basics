0d377f1bf79b8db63401f85d4011ea45832b7e42
Eduard555 <ololohov@gmail.com>
2020-10-13 21:49:38 +0500

    HW 4.3 Trucks fixed

--- 04_NumbersStringsAndDates/TrucksContainersAndBoxes/src/Main.java
+++ 04_NumbersStringsAndDates/TrucksContainersAndBoxes/src/Main.java
@@ -1,27 +1,45 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        int box = 550;
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите количество ящиков!");
+        Integer box = scanner.nextInt();
 
         int cont = 1;
         int i = 0;
         int y = 0;
+        int b = 1;
         int trucks = 1;
 
-        for (int b = 1; b <= box; b++) {
+        System.out.println("Грузовик № "+trucks);
+        System.out.println("\tКонтейнер № "+cont);
+        System.out.println("\t\t\tЯщик № "+b);
+
+        for ( b = 1; b <= box; b++) {
             i ++;
             y ++;
-            if (i > 27){
-                cont ++;
-                i = 1;
-            }
             if (y > 324){
                 trucks ++;
+                System.out.println("Грузовик №: "+trucks);
                 y = 1;
             }
-            System.out.println("В грузовике "+trucks+" находится контейнер "+cont+" и ящик номер "+b);
+            if (i > 27){
+                cont ++;
+                System.out.println("\tКонтейнер №: "+cont);
+                i = 1;
+            }
+            System.out.println("\t\t\tЯщик №: "+b);
 
         }
+        float allC = (float) (box / 27.00);
+        float allT = (float) (box / 324.00);
+        int c = (int) Math.ceil(allC);
+        int t = (int) Math.ceil(allT);
+
+        System.out.println("Необходимо:");
+        System.out.println("\tГрузовиков: "+t+" шт.");
+        System.out.println("\tКонтейнеров: "+c+" шт.");
     }
 }
