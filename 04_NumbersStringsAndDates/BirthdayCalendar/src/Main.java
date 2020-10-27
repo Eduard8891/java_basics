eff0225a3fb1943d1472cbc714f369f82145d1c5
Eduard555 <ololohov@gmail.com>
2020-10-28 02:43:51 +0500

    HW 4.6 BirthdayCalendar add*

--- 04_NumbersStringsAndDates/BirthdayCalendar/src/Main.java
+++ 04_NumbersStringsAndDates/BirthdayCalendar/src/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         int day = 17;
-        int month = 3;
+        int month = 4;
         int year = 1988;
  
         collectBirthdays(year, month, day);
@@ -19,15 +19,17 @@ public class Main {
     public static String collectBirthdays(int year, int month, int day) throws Exception {
 
 
-        DateFormat dateformat  = new SimpleDateFormat("dd.MM.yyyy EEEE");
+        DateFormat dateformat  = new SimpleDateFormat("dd.MM.yyyy");
         Calendar cal = Calendar.getInstance();
         cal.set(Calendar.DAY_OF_MONTH, day);
+        String [] dayOfWeek = {"000", "Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота",};
         int j = cal.get(Calendar.YEAR) - year;
         cal.set(Calendar.YEAR, year);
-        cal.set(Calendar.MONTH, month);
+        cal.set(Calendar.MONTH, month-1);
         for (int i=0; i <= j; i++ ){
             cal.set(Calendar.YEAR, year+i);
-            System.out.println(i+" - "+dateformat.format(cal.getTime()));
+            int k = cal.get(Calendar.DAY_OF_WEEK);
+            System.out.println(i+" - "+dateformat.format(cal.getTime())+" "+dayOfWeek[k]);
         }
         return "";
     }
