ebc03d3466a1983c30026e7e24291f729a47dca3
Eduard555 <ololohov@gmail.com>
2020-12-02 17:08:17 +0500

    Homework 7.1

--- 07_AdvancedOOPFeatures/LambdaExpressions/src/Main.java
+++ 07_AdvancedOOPFeatures/LambdaExpressions/src/Main.java
@@ -10,11 +10,25 @@ public class Main
     private static String staffFile = "data/staff.txt";
     private static String dateFormat = "dd.MM.yyyy";
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         ArrayList<Employee> staff = loadStaffFromFile();
+        Collections.sort(staff, (o1, o2) ->
+        {
+            if (o1.getSalary().equals(o2.getSalary()))
+            {
+                return o1.getName().compareTo(o2.getName());
+            }
+            else
+            {
+                return o1.getSalary().compareTo(o2.getSalary());
+            }
+        });
 
 
+        for (Employee employee : staff)
+        {
+            System.out.println(employee);
+        }
     }
 
     private static ArrayList<Employee> loadStaffFromFile()
