8a7f6ac9d149d2b36cda950c67484c9aa678a3d3
Eduard555 <ololohov@gmail.com>
2020-12-10 16:15:57 +0500

    7.2.2 fix Calendar

--- 07_AdvancedOOPFeatures/Airport/src/Main.java
+++ 07_AdvancedOOPFeatures/Airport/src/Main.java
@@ -1,25 +1,22 @@
-import com.skillbox.airport.Aircraft;
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
-import com.skillbox.airport.Terminal;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Date;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        Date curTime = new Date(System.currentTimeMillis());
-        Date addTwoHours = new Date(System.currentTimeMillis() + 7200000);
+        Calendar cal = new GregorianCalendar();
+        Calendar cal2 = new GregorianCalendar();
+        cal2.add(Calendar.HOUR, 2);
+        Date curTime = cal.getTime();
+        Date curTimeAfter = cal2.getTime();
 
 
         Airport.getInstance().getTerminals()
                 .stream().flatMap(t -> t.getFlights()
                 .stream().filter(flight -> flight.getDate().after(curTime) &
-                        flight.getDate().before(addTwoHours) & flight.getType() == Flight.Type.ARRIVAL)
+                        flight.getDate().before(curTimeAfter) & flight.getType() == Flight.Type.ARRIVAL)
                 .map(Flight::toString))
                 .forEach(System.out::println);
 
