a484275ea712e06dc28c250a59e74a1fcc077b3c
Eduard555 <ololohov@gmail.com>
2020-12-14 18:31:46 +0500

    Homework 8.1 CustomerList

--- 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/CustomerStorage.java
+++ 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/CustomerStorage.java
@@ -11,9 +11,20 @@ public class CustomerStorage
 
     public void addCustomer(String data)
     {
-        String[] components = data.split("\\s+");
-        String name = components[0] + " " + components[1];
-        storage.put(name, new Customer(name, components[3], components[2]));
+        try {
+            String[] components = data.split("\\s+");
+            if (components.length == 4) {
+                if (components[2].matches("^[A-Za-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,6}$")) {
+                    if (components[3].matches("[+]+[0-9]+")) {
+                        String name = components[0] + " " + components[1];
+                        storage.put(name, new Customer(name, components[3], components[2]));
+                    }
+                    else throw new IllegalArgumentException("You entered invalid phone number! Try again.");
+                } else throw new IllegalArgumentException("You entered invalid email! Try again.");
+            } else throw new IllegalArgumentException("You entered extra data! Try again.");
+        } catch (RuntimeException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 
     public void listCustomers()
@@ -23,7 +34,9 @@ public class CustomerStorage
 
     public void removeCustomer(String name)
     {
-        storage.remove(name);
+        if (storage.containsKey(name)) {
+            storage.remove(name);
+        } else throw new IllegalArgumentException("Sorry, this name this not on the list!");
     }
 
     public int getCount()
