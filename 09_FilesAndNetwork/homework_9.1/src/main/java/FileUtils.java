7fb5200d26abeee5c0d25531425d5b2c15481a99
Eduard555 <ololohov@gmail.com>
2021-01-20 02:12:43 +0500

    Homework 9.1

--- 09_FilesAndNetwork/homework_9.1/src/main/java/FileUtils.java
+++ 09_FilesAndNetwork/homework_9.1/src/main/java/FileUtils.java
@@ -1,6 +1,44 @@
+import java.io.File;
+
 public class FileUtils {
 
+    private static long total = 0;
+
     public static long calculateFolderSize(String path) {
-        return 0;
+
+        try {
+            File folder = new File(path);
+            File[] files = folder.listFiles();
+            for (File file : files) {
+                if (file.isDirectory())
+                {
+                    calculateNestedFolders(path+"/"+file.getName());
+                }
+                System.out.println(file.getName()+": "+file.length()+" byte");
+                total += file.length();
+            }
+            total = total/1048576;
+            System.out.println("Общий размер всех файлов и папок в \""+path+"\" равен "+total+" МБ");
+            return total;
+        }
+        catch (NullPointerException ex) {
+            System.out.println("По этому пути ничего не найдено! Попробуйте еще раз.");
+        }
+        return total;
+    }
+
+    public static void calculateNestedFolders(String pathNestedFolders)
+    {
+        File nestedFolder = new File(pathNestedFolders);
+        File[] nestedFiles = nestedFolder.listFiles();
+        for (File file : nestedFiles)
+        {
+            if (file.isDirectory())
+            {
+                calculateNestedFolders(pathNestedFolders+"/"+file.getName());
+            }
+            System.out.println("\t"+file.getName()+": "+file.length()+" byte");
+            total += file.length();
+        }
     }
 }
--- 09_FilesAndNetwork/homework_9.1/src/main/java/Main.java
+++ 09_FilesAndNetwork/homework_9.1/src/main/java/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите путь к папке! (например: \"c:/games/etc\")");
+
+        while (true) {
+
+            String input = scanner.nextLine();
+
+            if (input.matches("^[A-Za-z]:/(\\w+(/)*)+")) {
+
+                FileUtils.calculateFolderSize(input);
+
+            } else System.out.println("Неверный путь! Попробуйте еще раз.");
+        }
     }
 }
