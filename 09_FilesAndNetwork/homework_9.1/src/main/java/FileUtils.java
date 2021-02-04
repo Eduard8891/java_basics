cd88e95989f183eede9599f1cf56800ec4406c8e
Eduard555 <ololohov@gmail.com>
2021-02-04 17:49:20 +0500

    Homework 9.1 fix

--- 09_FilesAndNetwork/homework_9.1/src/main/java/FileUtils.java
+++ 09_FilesAndNetwork/homework_9.1/src/main/java/FileUtils.java
@@ -1,44 +1,22 @@
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.stream.Stream;
 
-public class FileUtils {
+public class FileUtils extends SimpleFileVisitor{
 
-    private static long total = 0;
+    public static long calculateFolderSize(String path) throws IOException {
 
-    public static long calculateFolderSize(String path) {
+        long size = Files.walk(Paths.get(path))
+                .map(Path::toFile)
+                .filter(File::isFile)
+                .mapToLong(File::length)
+                .sum();
 
-        try {
-            File folder = new File(path);
-            File[] files = folder.listFiles();
-            for (File file : files) {
-                if (file.isDirectory())
-                {
-                    calculateNestedFolders(path+"/"+file.getName());
-                }
-                System.out.println(file.getName()+": "+file.length()+" byte");
-                total += file.length();
-            }
-            total = total/1048576;
-            System.out.println("Общий размер всех файлов и папок в \""+path+"\" равен "+total+" МБ");
-            return total;
-        }
-        catch (NullPointerException ex) {
-            System.out.println("По этому пути ничего не найдено! Попробуйте еще раз.");
-        }
-        return total;
-    }
+        long sizeTotal = size/1048576;
 
-    public static void calculateNestedFolders(String pathNestedFolders)
-    {
-        File nestedFolder = new File(pathNestedFolders);
-        File[] nestedFiles = nestedFolder.listFiles();
-        for (File file : nestedFiles)
-        {
-            if (file.isDirectory())
-            {
-                calculateNestedFolders(pathNestedFolders+"/"+file.getName());
-            }
-            System.out.println("\t"+file.getName()+": "+file.length()+" byte");
-            total += file.length();
-        }
+        System.out.println("Общий размер всех файлов и папок в пути " + path +" : "+ + sizeTotal + " Мб");
+
+        return size;
     }
 }
--- 09_FilesAndNetwork/homework_9.1/src/main/java/Main.java
+++ 09_FilesAndNetwork/homework_9.1/src/main/java/Main.java
@@ -1,8 +1,13 @@
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите путь к папке! (например: \"c:/games/etc\")");
@@ -13,6 +18,7 @@ public class Main {
 
             if (input.matches("^[A-Za-z]:/(\\w+(/)*)+")) {
 
+//                Path inputPath = Paths.get(input);
                 FileUtils.calculateFolderSize(input);
 
             } else System.out.println("Неверный путь! Попробуйте еще раз.");
