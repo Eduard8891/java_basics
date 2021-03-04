d0b3fb0ebd2ac83fd9c7ec7547669e57975561c4
Eduard555 <ololohov@gmail.com>
2021-03-04 20:21:10 +0500

    I need help

--- 09_FilesAndNetwork/homework_9.2/src/main/java/FileUtils.java
+++ 09_FilesAndNetwork/homework_9.2/src/main/java/FileUtils.java
@@ -1,5 +1,35 @@
+import com.puppycrawl.tools.checkstyle.Main;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
 public class FileUtils {
-    public static void copyFolder(String sourceDirectory, String destinationDirectory) {
-        // TODO: write code copy content of sourceDirectory to destinationDirectory
+
+
+    public static void copyFolder(String sourceDirectory, String destinationDirectory) throws IOException {
+        File source = new File(sourceDirectory);
+        File dest = new File(destinationDirectory);
+        copyFiles(source, dest);
+    }
+
+
+
+
+
+    public static void copyFiles (File source, File dest) throws IOException {
+
+        Files.copy(source.toPath(), dest.toPath());
+
+        if (source.isDirectory()) {
+            File []files = source.listFiles();
+            for (File s : files) {
+                Files.copy(s.toPath(), dest.toPath());
+                if (s.isFile()) dest = new File(dest.toPath()+"/"+s.getName());
+            }
+        }
     }
 }
--- 09_FilesAndNetwork/homework_9.2/src/main/java/Main.java
+++ 09_FilesAndNetwork/homework_9.2/src/main/java/Main.java
@@ -1,4 +1,26 @@
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите путь к папке!");
+
+        String firstPath = scanner.nextLine();
+
+        if (firstPath.matches("^[A-Za-z]:/(\\w+(/)*)+")) {
+            System.out.println("Введите путь для копирования папки!");
+            String secondPath = scanner.nextLine();
+
+
+            if (secondPath.matches("^[A-Za-z]:/(\\w+(/)*)+")) {
+                FileUtils.copyFolder(firstPath, secondPath);
+
+            } else System.out.println("Неверный путь! Попробуйте еще раз.");
+
+        } else System.out.println("Неверный путь! Попробуйте еще раз.");
     }
 }
