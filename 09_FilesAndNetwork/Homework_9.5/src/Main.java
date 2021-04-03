36c7dd66c8e868b5ef7c046e1a2cb5829f29b0ac
Eduard Sharipov <ololohov@gmail.com>
2021-04-03 19:07:05 +0500

    HomeWork 9.5 fix (optimized)

--- 09_FilesAndNetwork/Homework_9.5/src/Main.java
+++ 09_FilesAndNetwork/Homework_9.5/src/Main.java
@@ -1,126 +1,29 @@
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.*;
-import java.util.List;
+
+import java.io.*;
 
 public class Main {
-    static HashMap <String, String> colors = new HashMap<>();
-    private static String jsonPath = "src/map.json";
-    static {
-    Parsing.putColor();
-    }
-    static ArrayList <Station> stations = new ArrayList<>();
-    static ArrayList <Line> lines = new ArrayList<>();
-    static ArrayList <Connection> connections = new ArrayList<>();
-    static JSONArray connectFinish = new JSONArray();
-
-
     public static void main(String[] args) throws IOException, ParseException {
+        wikiMskMetroJSON doc = new wikiMskMetroJSON();
+        doc.parseFile("src/metro.html");
+        JSONObject obj = new JSONObject();
+        obj.put("connections", doc.connections);
+        obj.put("stations", doc.stations);
+        obj.put("lines", doc.lines);
+        FileWriter file = new FileWriter("src/map.json");
+        obj.writeJSONString(file);
+        file.flush();
+        file.close();
 
-        String filePath = parseFile("src/metro.html");
-        Document document = Jsoup.parse(filePath);
-        Parsing.parseLine(document);
-        Parsing.parseStation(document);
-        Parsing.parseConnections(document);
-        createGSonFile();
-        parseJson();
-    }
-
-    public static void parseJson() throws ParseException, IOException {
-        ArrayList <Integer> stationsValues = new ArrayList<>();
         JSONParser parser = new JSONParser();
-        Object object = parser.parse(new FileReader(jsonPath));
-        JSONObject jsonObject = (JSONObject) object;
-        JSONObject parseStations = (JSONObject) jsonObject.get("stations");
-
-            for (Object parseValue: parseStations.values()) {
-                String temp = parseValue.toString()
-                        .replaceAll("\"", "");
-                String[] numValues = temp.split(",");
-                stationsValues.add(numValues.length);
-            }
-            int count = 0;
-            for (Object parseLines: parseStations.keySet()) {
-                System.out.println(parseLines.toString() + ": " + stationsValues.get(count) + " станций");
-                count++;
-            }
+        Reader reader = new BufferedReader(new FileReader("src/map.json"));
+        Object object = parser.parse(reader);
+        JSONObject parsingObj = (JSONObject) object;
+        Object connections = parsingObj.get("connections");
+        String [] connect = connections.toString().split("]");
+        System.out.println("Количество переходов: " + connect.length);
     }
 
-    public static void createGSonFile() throws IOException {
-        JSONObject generalObject = new JSONObject();
-        JSONObject stationsOnLine = new JSONObject();
-
-        for (int i = 0; i < lines.size(); i++) {              //Stations
-            JSONArray jArrayStations = new JSONArray();
-            for (Station st: stations) {
-                if (st.getLine().getNumber() == lines.get(i).getNumber())
-                    jArrayStations.add(st.getName());
-            }
-            stationsOnLine.put(lines.get(i).getName(), jArrayStations);
-        }
-
-        JSONArray jArrayLines = new JSONArray();              //Lines
-        for (Line l: lines) {
-            JSONObject jObj = new JSONObject();
-            jObj.put("Number", l.getNumber());
-            jObj.put("Name", l.getName());
-            jObj.put("Color", l.getColor());
-            jArrayLines.add(jObj);
-            generalObject.put("Lines", jArrayLines);
-        }
-        generalObject.put("stations", stationsOnLine);
-
-
-        Object temp = null;
-        JSONObject obj;
-        JSONArray array = null;
-        for (Connection c : connections) {
-            if (!c.getStationFrom().equals(temp)) {
-                connectFinish.add(array);
-                array = new JSONArray();
-                obj = new JSONObject();
-                obj.put("Line: ", c.getLineFrom().getName());
-                obj.put("Station: ", c.getStationFrom().getName());
-                array.add(obj);
-                obj = new JSONObject();
-                obj.put("Line: ", c.getLineTo().getName());
-                obj.put("Station: ", c.getStationTo().getName());
-                array.add(obj);
-
-            }
-            else if (!array.contains(c.getStationTo())) {
-                obj = new JSONObject();
-                obj.put("Line: ", c.getLineTo().getName());
-                obj.put("Station: ", c.getStationTo().getName());
-                array.add(obj);
-            }
-            temp = c.getStationFrom();
-        }
-        connectFinish.remove(0);
-        generalObject.put("connections", connectFinish);
-
-
-
-
-        FileWriter writer = new FileWriter(jsonPath);
-        writer.write(generalObject.toString());
-        writer.flush();
-    }
-
-
-    public static String parseFile(String filePath) throws IOException {
-        StringBuilder sb = new StringBuilder();
-        List<String> list = Files.readAllLines(Path.of(filePath));
-        list.forEach(line -> sb.append(line).append("\n"));
-        return sb.toString();
-    }
 }
