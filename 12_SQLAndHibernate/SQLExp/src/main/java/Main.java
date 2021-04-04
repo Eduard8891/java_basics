a4241d0d478972d0ba6963903dac7fa3a5eb23ae
Eduard Sharipov <ololohov@gmail.com>
2021-04-04 14:21:14 +0500

    Homework SQLEXP 12.1 (only SELECT)

--- 12_SQLAndHibernate/SQLExp/src/main/java/Main.java
+++ 12_SQLAndHibernate/SQLExp/src/main/java/Main.java
@@ -5,7 +5,6 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) {
-        HashMap <String, Integer> prof = new HashMap<String, Integer>();
         String url = "jdbc:mysql://localhost:3306/skillbox";
         String user = "root";
         String pass = "testtest";
@@ -13,26 +12,11 @@ public class Main {
         try {
             Connection connection = DriverManager.getConnection(url, user, pass);
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT Courses.name AS name, Subscription_date AS date " +
-                    "FROM Courses JOIN Subscriptions ON Courses.id = Subscriptions.course_id ORDER BY Subscription_date");
-            String name;
-            int count;
-            double maxMonth = 0;
+            ResultSet resultSet = statement.executeQuery("SELECT Courses.name, " +
+                    "COUNT(MONTH(Subscriptions.subscription_date))/MAX(MONTH(Subscriptions.subscription_date)) FROM Courses " +
+                    "JOIN Subscriptions ON Subscriptions.course_id = Courses.id GROUP BY Courses.name;");
             while (resultSet.next()) {
-                name = resultSet.getString(1);
-                String[] date = resultSet.getString(2).split("-");
-                if (maxMonth < Double.parseDouble(date [1])) maxMonth = Double.parseDouble(date [1]);
-                if (prof.containsKey(name)) {
-                    count =  prof.get(name);
-                    prof.replace(name,count+1);
-                }
-                else prof.put(name, 1);
-            }
-            double arifmeticMean;
-            for (Map.Entry a: prof.entrySet()) {
-                arifmeticMean = (int)a.getValue()/maxMonth;
-                System.out.printf(a.getKey() + " - "+"%.2f", arifmeticMean);
-                System.out.println("");
+                System.out.println(resultSet.getString(1) +" - "+ resultSet.getDouble(2));
             }
         } catch (SQLException ex) {
             ex.printStackTrace();
